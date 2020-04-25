ab369fc541cb88ce00a45bad4e446e01f473a2e6
Kulbir Saini <saini@saini.co.in>
2020-04-25 18:13:06 +0530

    Enable/Disable Delete All menu item depending on the entries. Added gradle settings file.

--- app/src/main/java/com/gofedora/myreminder/fragments/ReminderListFragment.kt
+++ app/src/main/java/com/gofedora/myreminder/fragments/ReminderListFragment.kt
@@ -6,9 +6,9 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.gofedora.myreminder.R
 import com.gofedora.myreminder.ReminderListAdapter
 import com.gofedora.myreminder.ReminderViewModel
-import com.gofedora.myreminder.R
 import kotlinx.android.synthetic.main.reminder_list.*
 
 /**
@@ -32,6 +32,18 @@ class ReminderListFragment : Fragment() {
         super.onCreateOptionsMenu(menu, inflater)
     }
 
+    override fun onPrepareOptionsMenu(menu: Menu) {
+        val reminderViewModel = ViewModelProvider(this).get(ReminderViewModel::class.java)
+
+        reminderViewModel.allReminders.observe(viewLifecycleOwner, Observer { reminders ->
+            reminders?.let {
+                menu.findItem(R.id.actionDeleteAll).isEnabled = reminders.isNotEmpty()
+            }
+        })
+
+        super.onPrepareOptionsMenu(menu)
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.reminder_list, container, false)
@@ -46,7 +58,9 @@ class ReminderListFragment : Fragment() {
         }
 
         reminderViewModel.allReminders.observe(viewLifecycleOwner, Observer { reminders ->
-            reminders?.let { reminderListAdapter.setReminders(reminders) }
+            reminders?.let {
+                reminderListAdapter.setReminders(reminders)
+            }
         })
 
         remindersList.apply {
